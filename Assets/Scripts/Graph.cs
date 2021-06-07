7ce878b3d8bd6441bb3bf382bb63c7418320b2e5
KamenRiderHibiki <1269236499@qq.com>
2021-06-07 18:19:54 +0800

    0607做scene2

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,18 +10,21 @@ public class Graph : MonoBehaviour
     int resolution = 10;
     [SerializeField, Range(2, 10)]
     float width = 2;
+    Transform[] points;
     void Awake() {
         var position = Vector3.zero;
         var step = width / resolution;
         var scale = Vector3.one * step;
+        points = new Transform[resolution];
         for(int i = 0;i < resolution;i++)
         {
             Transform point = Instantiate(pointPrefab);
             point.SetParent(transform, false);
             position.x = (i + 0.5f) * step - width / 2;
-            position.y = position.x * position.x;
+            position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            points[i] = point;
         }
     }
     // Start is called before the first frame update
@@ -33,6 +36,12 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float time = Time.time;
+        for(int i = 0; i < resolution; i++){
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
     }
 }
