f50ba125a0359757012e0a02f2b07eb1fe07515c
KamenRiderHibiki <1269236499@qq.com>
2021-06-16 23:57:02 +0800

    continue 4 finish fps show

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -5,17 +5,41 @@ public class FrameRateCounter : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI display = default;
-    
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+    int frames;
+    float duration, bestDuration, worstDuration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        frames = 0;
+        duration = 0;
+        bestDuration = float.MaxValue;
+        worstDuration = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         float frameDuration = Time.unscaledDeltaTime;
-        display.SetText("FSP\n{0:0}\n000\n000", 1f / frameDuration);
+        frames++;
+        duration += frameDuration;
+
+        if(frameDuration < bestDuration){
+            bestDuration = frameDuration;
+        }
+        if(frameDuration > worstDuration){
+            worstDuration = frameDuration;
+        }
+        if(duration >= sampleDuration){
+            display.SetText("FPS\n{0:0}\n{1:0}\n{2:0}", 1f / bestDuration,
+            frames / duration,
+            1f / worstDuration
+            );
+            frames = 0;
+            duration = 0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0f;
+        }
     }
 }
