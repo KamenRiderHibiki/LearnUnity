3dd1669d11e44c7f78862720e03343df0ec6c0ba
KamenRiderHibiki <1269236499@qq.com>
2021-11-24 22:54:31 +0800

    1.2.2做前半

--- Assets/Scripts/GetTestProp.cs
+++ Assets/Scripts/GetTestProp.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using TMPro;
 
@@ -16,7 +17,25 @@ public class GetTestProp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = testObject.GetComponent<MovingSphere>().velocity;
+        Vector3 dir = Vector3.zero;
+        dir = GetSphereVelocity();
         display.SetText("Val\n{0:3}\n{1:3}\n{2:3}", dir.x, dir.y, dir.z);
+    }
+
+    Vector3 GetSphereVelocity()
+    {
+        MovingSphere movScript = null;
+        RigidbodySphere rigScript = null;
+        movScript = testObject.GetComponent(typeof(MovingSphere)) as MovingSphere;
+        if (movScript != null)
+        {
+            return movScript.velocity;
+        }
+        rigScript = testObject.GetComponent(typeof(RigidbodySphere)) as RigidbodySphere;
+        if (rigScript != null)
+        {
+            return rigScript.velocity;
+        }
+        return Vector3.zero;
     }
 }
